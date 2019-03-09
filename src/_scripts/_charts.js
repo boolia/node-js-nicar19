ebcb23f8fc0e540c66ff81d6d3617ae64efd3a6a
boolia <jdonheiser+renee@gmail.com>
2019-03-09 14:33:22 -0800

    Set up container and chart dimensions

--- src/_scripts/_charts.js
+++ src/_scripts/_charts.js
@@ -1,4 +1,19 @@
 var d3 = require('d3');
 
+// prevents clipping
+var margin = {top: 20, right: 20, bottom:20, left:40};
+
 var container = d3.select('#county-homicides');
-var svg = container.append('svg');
+var containerWidth = container.node().offsetWidth;
+var containerHeight = containerWidth * 0.66;
+
+var chartWidth = containerWidth - margin.right - margin.left;
+var chartHeight = containerHeight - margin.top - margin.bottom;
+
+var svg = container.append('svg')
+				.attr('width', containerWidth)
+				.attr('height', containerHeight)
+				.append('g')
+				    .attr('transform', `translate(${margin.left}, ${margin.top})`)
+;
+
