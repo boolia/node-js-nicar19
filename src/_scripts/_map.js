6cd267f7dd3020f4b10ac84c32f779f6d4d6b012
boolia <jdonheiser+renee@gmail.com>
2019-03-09 16:54:21 -0800

    Draw map and markers

--- src/_scripts/_map.js
+++ src/_scripts/_map.js
@@ -1,1 +1,9 @@
-_
+var map = L.map('map') // don't need # because leaflet accounts
+// satellite view
+var sat = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v9/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibGF0aW1lcyIsImEiOiJjanJmNjg4ZzYweGtvNDNxa2ZpZ2lma3Z4In0.g0lYVIEs9Y5QcUOhXactHA');
+sat.addTo(map); // choose tiles
+map.setView([33.983265, -118.306799], 18); // set intial view, second value is zoom level
+
+homicides.forEach(obj =>
+	L.circleMarker([obj.latitude,  obj.longitude])
+    .addTo(map));
